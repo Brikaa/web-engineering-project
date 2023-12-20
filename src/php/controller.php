83b8aca3ec3cd33f5312e6ae9a58acbf1cebaa5b
Omar Brikaa <brikaaomar@gmail.com>
2023-12-20 16:58:21 +0200

    Lift connection object up

--- src/php/controller.php
+++ src/php/controller.php
@@ -19,59 +19,58 @@ class DbController
   }
 
   public function signup(
+    mysqli $con,
     string $email,
     string $name,
     string $password,
     string $telephone
   ) {
-    if ($this->repo->select_user_by_name_or_email($name, $email) != null) {
+    if ($this->repo->select_user_by_name_or_email($con, $name, $email) != null) {
       throw new Exception("A user with this name or email already exists");
     }
     $req = new InsertUserRequest($email, $name, $password, $telephone, "", 0.0);
-    $this->repo->insert_user($req);
+    $this->repo->insert_user($con, $req);
   }
 
   public function login(
+    mysqli $con,
     string $email,
     string $password
   ) {
-    $user = $this->repo->select_user_by_email_and_password($email, $password);
+    $user = $this->repo->select_user_by_email_and_password($con, $email, $password);
     if ($user == null)
       throw new Exception("Invalid email or password");
     $this->session[USER_ID] = $user->id;
   }
 
-  public function get_logged_in_user_context()
+  public function get_logged_in_user_context(mysqli $con)
   {
     $user_id = $this->session[USER_ID];
-    return $this->repo->select_user_by_id($user_id);
+    return $this->repo->select_user_by_id($con, $user_id);
   }
 
   public function update_user(
+    mysqli $con,
     UserContext $ctx,
     InsertUserRequest $req
   ) {
-    $this->repo->update_user_by_id($ctx->id, $req);
+    $this->repo->update_user_by_id($con, $ctx->id, $req);
   }
 
   public function update_passenger(
+    mysqli $con,
     UserContext $ctx,
     string $passport_image_url
   ) {
-    $this->repo->update_passenger_by_user_id($ctx->id, $passport_image_url);
+    $this->repo->update_passenger_by_user_id($con, $ctx->id, $passport_image_url);
   }
 
   public function update_company(
+    mysqli $con,
     UserContext $ctx,
     string $bio,
     string $address
   ) {
-    $this->repo->update_company_by_user_id($ctx->id, $bio, $address);
+    $this->repo->update_company_by_user_id($con, $ctx->id, $bio, $address);
   }
 }
-
-$create_db_controller_constructor = function (Repo $repo, array &$session): Closure {
-  return function () use ($repo, &$session): DbController {
-    return new DbController($repo, $session);
-  };
-};
--- src/php/repo.php
+++ src/php/repo.php
@@ -5,30 +5,17 @@ require_once 'model.php';
 
 class Repo
 {
-  private mysqli $con;
-
-  public function __construct()
+  private function with_prepared_statement(mysqli $con, string $sql, $fn)
   {
-    $this->con = mysqli_connect("wep-db", "user", "user123", "app");
-  }
-
-  function __destruct()
-  {
-    $this->con->close();
-  }
-
-
-  private function with_prepared_statement(string $sql, $fn)
-  {
-    $stmt = $this->con->prepare($sql);
+    $stmt = $con->prepare($sql);
     $res = $fn($stmt);
     $stmt->close();
     return $res;
   }
 
-  private function with_executed_statement(string $sql, string $binding, array $params, Closure $fn)
+  private function with_executed_statement(mysqli $con, string $sql, string $binding, array $params, Closure $fn)
   {
-    return $this->with_prepared_statement($sql, function ($s) use (&$binding, &$params, $fn) {
+    return $this->with_prepared_statement($con, $sql, function ($s) use (&$binding, &$params, $fn) {
       $s->bind_param($binding, ...$params);
       if ($s->execute()) {
         return $fn($s);
@@ -37,16 +24,16 @@ class Repo
     });
   }
 
-  private function execute_statement(string $sql, string $binding, array $params): ?bool
+  private function execute_statement(mysqli $con, string $sql, string $binding, array $params): ?bool
   {
-    return $this->with_executed_statement($sql, $binding, $params, function ($s) {
+    return $this->with_executed_statement($con, $sql, $binding, $params, function ($s) {
       return $s != null && $s != false;
     });
   }
 
-  private function select_one(string $sql, string $binding, array $params): ?array
+  private function select_one(mysqli $con, string $sql, string $binding, array $params): ?array
   {
-    return $this->with_executed_statement($sql, $binding, $params, function (mysqli_stmt $s) {
+    return $this->with_executed_statement($con, $sql, $binding, $params, function (mysqli_stmt $s) {
       if ($res = $s->get_result()) {
         return $res->fetch_array();
       }
@@ -54,9 +41,9 @@ class Repo
     });
   }
 
-  private function select_all(string $sql, string $binding, array $params): array
+  private function select_all(mysqli $con, string $sql, string $binding, array $params): array
   {
-    return $this->with_executed_statement($sql, $binding, $params, function (mysqli_stmt $s) {
+    return $this->with_executed_statement($con, $sql, $binding, $params, function (mysqli_stmt $s) {
       $arr = array();
       while ($res = $s->get_result()) {
         if ($e = $res->fetch_array()) {
@@ -67,9 +54,14 @@ class Repo
     });
   }
 
-  private function select_user_by_condition(string $condition, string $binding, array $params): ?UserContext
-  {
+  private function select_user_by_condition(
+    mysqli $con,
+    string $condition,
+    string $binding,
+    array $params
+  ): ?UserContext {
     $user = $this->select_one(
+      $con,
       "SELECT User.id, User.name, Passenger.id, Company.id FROM User
       LEFT JOIN Company ON Company.user_id = User.user_id
       LEFT JOIN Passenger ON Passenger.user_id = User.user_id
@@ -92,12 +84,14 @@ class Repo
   }
 
   private function select_flights_summaries_by_condition(
+    mysqli $con,
     string $from,
     string $condition,
     string $bindings,
     array $params,
   ): array {
     $arr = $this->select_all(
+      $con,
       "SELECT
         Flight.id,
         Flight.name,
@@ -134,10 +128,11 @@ class Repo
   }
 
   public function insert_user(
-
+    mysqli $con,
     InsertUserRequest $user
   ): bool {
     return $this->execute_statement(
+      $con,
       "INSERT INTO User(`email`, `name`, `password`, `telephone`, `photo_url`, `money`) values(?, ?, ?, ?, ?, ?)",
       "sssssd",
       [$user->email, $user->name, $user->password, $user->telephone, $user->photo_url, $user->money]
@@ -145,11 +140,13 @@ class Repo
   }
 
   public function insert_passenger_for_user_id(
+    mysqli $con,
 
     string $user_id,
     string $passport_image_url
   ): bool {
     return $this->execute_statement(
+      $con,
       "INSERT INTO Passenger(`user_id`, `passport_image_url`) values(?, ?)",
       "ss",
       [$user_id, $passport_image_url]
@@ -157,12 +154,13 @@ class Repo
   }
 
   public function insert_company_for_user_id(
-
+    mysqli $con,
     string $user_id,
     string $bio,
     string $address
   ): bool {
     return $this->execute_statement(
+      $con,
       "INSERT INTO Passenger(`user_id`, `bio`, `address`) values(?, ?, ?)",
       "sss",
       [$user_id, $bio, $address]
@@ -170,11 +168,13 @@ class Repo
   }
 
   public function update_user_by_id(
+    mysqli $con,
 
     string $id,
     InsertUserRequest $user
   ): bool {
     return $this->execute_statement(
+      $con,
       "UPDATE User SET `email`=?, `name`=?, `password`=?, `telephone`=?, `photo_url`=?, `money`=? WHERE id=?",
       "sssssds",
       [$user->email, $user->name, $user->password, $user->telephone, $user->photo_url, $user->money, $id]
@@ -182,11 +182,13 @@ class Repo
   }
 
   public function update_passenger_by_user_id(
+    mysqli $con,
 
     string $user_id,
     string $passport_image_url
   ): bool {
     return $this->execute_statement(
+      $con,
       "UPDATE Passenger SET passport_image_url=? WHERE user_id=?",
       "ss",
       [$passport_image_url, $user_id]
@@ -194,36 +196,39 @@ class Repo
   }
 
   public function update_company_by_user_id(
+    mysqli $con,
 
     string $user_id,
     string $bio,
     string $address
   ): bool {
     return $this->execute_statement(
+      $con,
       "UPDATE Company SET bio=?, `address`=? WHERE user_id=?",
       "sss",
       [$bio, $address, $user_id]
     );
   }
 
-  public function select_user_by_id(string $id)
+  public function select_user_by_id(mysqli $con, string $id)
   {
-    return $this->select_user_by_condition("User.id=?", "s", [$id]);
+    return $this->select_user_by_condition($con, "User.id=?", "s", [$id]);
   }
 
-  public function select_user_by_name_or_email(string $name, string $email)
+  public function select_user_by_name_or_email(mysqli $con, string $name, string $email)
   {
-    return $this->select_user_by_condition("User.name=? OR User.email=?", "ss", [$name, $email]);
+    return $this->select_user_by_condition($con, "User.name=? OR User.email=?", "ss", [$name, $email]);
   }
 
-  public function select_user_by_email_and_password(string $email, string $password)
+  public function select_user_by_email_and_password(mysqli $con, string $email, string $password)
   {
-    return $this->select_user_by_condition("User.email = ? AND User.password = ?", "ss", [$email, $password]);
+    return $this->select_user_by_condition($con, "User.email = ? AND User.password = ?", "ss", [$email, $password]);
   }
 
-  public function select_passenger_by_user_id(string $user_id): ?Passenger
+  public function select_passenger_by_user_id(mysqli $con, string $user_id): ?Passenger
   {
     $passenger = $this->select_one(
+      $con,
       "SELECT Passenger.user_id, Passenger.passport_image_url FROM Passenger WHERE Passenger.user_id = ?",
       "s",
       [$user_id]
@@ -234,9 +239,10 @@ class Repo
     return null;
   }
 
-  public function select_company_by_user_id(string $user_id): ?Company
+  public function select_company_by_user_id(mysqli $con, string $user_id): ?Company
   {
     $company = $this->select_one(
+      $con,
       "SELECT Company.user_id, Company.bio, Company.address FROM Company WHERE Company.user_id = ?",
       "s",
       [$user_id]
@@ -247,10 +253,11 @@ class Repo
     return null;
   }
 
-  public function select_upcoming_flights_summaries_for_user(string $user_id): array
+  public function select_upcoming_flights_summaries_for_user(mysqli $con, string $user_id): array
   {
     $now = date("Y-m-d H:i:s");
     return $this->select_flights_summaries_by_condition(
+      $con,
       "FlightReservation LEFT JOIN Flight ON FlightReservation.flight_id = Flight.id",
       "FlightReservation.passenger_user_id = ? AND StartCity.date_in_city > $now",
       "s",
@@ -258,10 +265,11 @@ class Repo
     );
   }
 
-  public function select_completed_flights_summaries_for_user(string $user_id): array
+  public function select_completed_flights_summaries_for_user(mysqli $con, string $user_id): array
   {
     $now = date("Y-m-d H:i:s");
     return $this->select_flights_summaries_by_condition(
+      $con,
       "FlightReservation LEFT JOIN Flight ON FlightReservation.flight_id = Flight.id",
       "FlightReservation.passenger_user_id = ? AND EndCity.date_in_city < $now",
       "s",
@@ -269,10 +277,11 @@ class Repo
     );
   }
 
-  public function select_available_flights_summaries_for_user(string $user_id): array
+  public function select_available_flights_summaries_for_user(mysqli $con, string $user_id): array
   {
     $now = date("Y-m-d H:i:s");
     return $this->select_flights_summaries_by_condition(
+      $con,
       "Flight",
       "StartCity.date_in_city > $now
       AND Flight.id NOT IN (
@@ -284,13 +293,14 @@ class Repo
   }
 
   public function select_available_flights_summaries_for_user_by_src_dest(
-
+    mysqli $con,
     string $user_id,
     string $src,
     string $dest
   ): array {
     $now = date("Y-m-d H:i:s");
     return $this->select_flights_summaries_by_condition(
+      $con,
       "Flight",
       "StartCity.date_in_city > $now
       AND Flight.id NOT IN (
@@ -304,11 +314,12 @@ class Repo
   }
 
   public function flight_reservation_by_user_id_and_flight_id_exists(
-
+    mysqli $con,
     string $user_id,
     string $flight_id
   ): bool {
     return $this->select_one(
+      $con,
       "SELECT FlightReservation.flight_id FROM FlightReservation
       WHERE
         FlightReservation.passenger_user_id = ?
@@ -318,9 +329,10 @@ class Repo
     ) != null;
   }
 
-  public function select_flight_details_by_flight_id(string $flight_id): ?FlightDetail
+  public function select_flight_details_by_flight_id(mysqli $con, string $flight_id): ?FlightDetail
   {
     $flights_with_cities = $this->select_all(
+      $con,
       "SELECT
         Flight.id,
         Flight.name,
@@ -358,36 +370,40 @@ class Repo
     }
   }
 
-  public function insert_flight_reservation_for_user(string $user_id, string $flight_id): bool
+  public function insert_flight_reservation_for_user(mysqli $con, string $user_id, string $flight_id): bool
   {
     return $this->execute_statement(
+      $con,
       "INSERT INTO FlightReservation(passenger_user_id, flight_id) VALUES (?, ?)",
       "ss",
       [$user_id, $flight_id]
     );
   }
 
-  public function delete_flight_reservation(string $flight_reservation_id): bool
+  public function delete_flight_reservation(mysqli $con, string $flight_reservation_id): bool
   {
     return $this->execute_statement(
+      $con,
       "DELETE FROM FlightReservation WHERE id = ?",
       "s",
       [$flight_reservation_id]
     );
   }
 
-  public function change_money_for_user(string $user_id, float $delta): bool
+  public function change_money_for_user(mysqli $con, string $user_id, float $delta): bool
   {
     return $this->execute_statement(
+      $con,
       "UPDATE User SET User.money = User.money + ? WHERE User.id = ?",
       "ds",
       [$delta, $user_id]
     );
   }
 
-  public function insert_message(string $sender_id, string $receiver_id, string $message)
+  public function insert_message(mysqli $con, string $sender_id, string $receiver_id, string $message)
   {
     return $this->execute_statement(
+      $con,
       "INSERT INTO `Message` (sender_user_id, receiver_user_id, `message`) VALUES (?, ?, ?)",
       "sss",
       [$sender_id, $receiver_id, $message]
